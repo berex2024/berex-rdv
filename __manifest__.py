42da58c43fc7d14304b6ff0bb32b676fb16041f5
berex2024 <rs@berex.be>
2026-07-20 11:48:02 +0200

    Add __manifest__.py for Berex RDV module

---
+++ __manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Berex RDV',
+    'version': '17.0.1.0.0',
+    'author': 'Berex',
+    'category': 'Website',
+    'depends': ['website', 'calendar', 'account'],
+    'data': [],
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
